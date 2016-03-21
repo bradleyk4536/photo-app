9fe7bbcdafc8e643d29650e76ec91e2ce7b435a1
Kenneth Bradley <k_bnet570@verizon.net>
2016-03-21 10:44:21 -0400

    Test

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
 	:address => 'smtp.sendgrid.net',
 	:port => '587',
 	:authentication => :plain,
-	:username => ENV['SENDGRID_USERNAME'],
-	:password => ENV['SENDGRID_PASSWORD'],
+	:username => 'BRADLEYKG4536',
+	:password => 'jetta@777',
 	:domain => 'heroku.com',
 	:enable_starttls_auto => true
 	}
