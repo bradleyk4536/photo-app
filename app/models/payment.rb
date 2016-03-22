f35752057d316807d5e279be84199ef2d8737d3f
Kenneth Bradley <k_bnet570@verizon.net>
2016-03-22 19:44:56 -0400

    fixed payment rb currency

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -19,6 +19,6 @@ class Payment < ActiveRecord::Base
 	#	setup payment for plan Premium
 	def process_payment
 		customer = Stripe::Customer.create email: email, card: token
-		Stripe::Charge.create customer: customer.id, amount: 1000, description: 'Premium', currency: 'USD'
+		Stripe::Charge.create customer: customer.id, amount: 1000, description: 'Premium', currency: 'usd'
 	end
 end
